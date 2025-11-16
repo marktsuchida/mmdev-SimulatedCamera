0701baaf1ef9a9d2db1103b16046d2e6ea740cdf
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-11-15 20:35:48 -0600

    Add benchmark for defocused Draw()
    Which is much slower due to the large Gaussian blue kernel.

--- bench_SimulatedSpecimen.cpp
+++ bench_SimulatedSpecimen.cpp
@@ -7,14 +7,16 @@
 
 void bm_simulated_specimen_draw(benchmark::State& state) {
     SimulatedSpecimen<std::uint16_t> specimen;
-    std::vector<std::uint16_t> buffer(512 * 512);
+    const double z_um = state.range(0);
+    const std::size_t width = 512, height = 512;
+    std::vector<std::uint16_t> buffer(width * height);
     auto *data = buffer.data();
     for ([[maybe_unused]] auto _ : state) {
-        specimen.Draw(data, 0.0, 0.0, 0.0, 512, 512, 0.2, 1000.0);
+        specimen.Draw(data, 0.0, 0.0, z_um, width, height, 0.2, 1000.0);
         benchmark::DoNotOptimize(data);
     }
 }
 
-BENCHMARK(bm_simulated_specimen_draw);
+BENCHMARK(bm_simulated_specimen_draw)->Arg(0)->Arg(50);
 
 BENCHMARK_MAIN();
