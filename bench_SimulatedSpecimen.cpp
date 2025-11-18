2fc5f4716d707cf8b9bfe20672645a403015cf16
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-11-17 21:26:21 -0600

    Add benchmarks for scalar vs SIMD Gaussian filter
    At this time the SIMD implementation (4-lane) is better than 4x faster
    on Rosetta 2 and (surprisingly) better than 7x faster on native arm64 on
    Apple M1 Pro.

--- bench_SimulatedSpecimen.cpp
+++ bench_SimulatedSpecimen.cpp
@@ -1,3 +1,4 @@
+#include "Gaussian2DFilter.h"
 #include "SimulatedSpecimen.h"
 
 #include <benchmark/benchmark.h>
@@ -21,5 +22,27 @@ BENCHMARK(bm_simulated_specimen_draw)
     ->Arg(0)
     ->Arg(50)
     ->Unit(benchmark::kMillisecond);
+
+template <auto Func> void bm_gaussian_2d_filter(benchmark::State &state) {
+    const std::size_t width = 512, height = 512;
+    std::vector<float> image(width * height, 42.0f);
+    auto *data = image.data();
+    for ([[maybe_unused]] auto _ : state) {
+        Func(data, width, height, 5.0);
+        benchmark::DoNotOptimize(data);
+    }
+}
+
+void bm_gaussian_2d_filter_scalar(benchmark::State &state) {
+    bm_gaussian_2d_filter<gaussian_internal::FastGaussian2DScalar<float>>(
+        state);
+}
+
+void bm_gaussian_2d_filter_simd(benchmark::State &state) {
+    bm_gaussian_2d_filter<gaussian_internal::FastGaussian2DSIMD>(state);
+}
+
+BENCHMARK(bm_gaussian_2d_filter_scalar)->Unit(benchmark::kMillisecond);
+BENCHMARK(bm_gaussian_2d_filter_simd)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
